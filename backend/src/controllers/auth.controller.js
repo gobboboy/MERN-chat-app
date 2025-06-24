65d462b6aa111bcc66ef854ff86a8eada8821559
Marcos Gobbo <marcoszgobbo@gmail.com>
2025-06-24 18:10:55 -0300

    Implementing JWT via cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,8 +3,10 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-    const { fullName, email, password } = req.body;
     try {
+        const { fullName, email, password } = req.body;
+
+        if (!fullName || !email || !password) return res.status(400).json({message: "All fields are required!"});
         if (password.length < 6) {
             return res.status(400).json({ message: "Password must be at least 6 characters" });
         }
